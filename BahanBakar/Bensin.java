803e6c75399028f49d0498b9dcc0e64180458d52
Nanda Rizki Nasution <nandarizki.nasution@gmail.com>
2019-10-24 12:21:06 +0700

    Update BahanBakar, adding folder Vehicle

---
+++ BahanBakar/Bensin.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Gasoline extends Petroleum{
+	private Integer octane;
+
+	public Integer getOctane() {
+		return octane;
+	}
+
+	public void setOctane(Integer octane) {
+		this.octane = octane;
+	}
+}
---
+++ BahanBakar/Diesel.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Diesel extends Petroleum{
+	private Integer cetane;
+
+	public Integer getCetane() {
+		return cetane;
+	}
+
+	public void setCetane(Integer cetane) {
+		this.cetane = cetane;
+	}
+}
